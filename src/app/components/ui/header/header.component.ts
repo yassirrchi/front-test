7ee89c7316dd0e358f497456382d9175a7310d52
yassirrchi <yassirrchi7@gmail.com>
2024-03-08 15:41:22 +0100

    initial commit

---
+++ src/app/components/ui/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Input } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  @Input() headerText!:String;
+
+}
